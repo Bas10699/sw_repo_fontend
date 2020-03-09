7ee4c6866c403c9490fde0b256b8c17f51bb987c
waen633 <komet633@gmail.com>
2020-03-09 23:12:29 +0700

    new_table_in_fromtype

--- src/component/FromTypeItem.js
+++ src/component/FromTypeItem.js
@@ -213,11 +213,15 @@ export default class FromTypeItem extends Component {
           </Col>
           <Col className="text-center">
             <Card >
+              <Card.Header  className=" bg-info">
               <h3 className="m-0 font-weight-bold text-dark ">
                 ตารางแสดงประเภทของอุปกรณ์ทั้งหมด
               </h3>
-           
-            <Table striped bordered hover variant="info">
+              </Card.Header>
+             
+           <Card.Body>
+
+           <Table striped bordered hover variant="info">
               <thead>
                 <tr>
                   <th>ลำดับ</th>
@@ -279,26 +283,7 @@ export default class FromTypeItem extends Component {
                               </OverlayTrigger>
                             ))}
                           </ButtonToolbar>
-                          {/* <Accordion defaultActiveKey="0">
-                            <Accordion.Toggle
-                              as={Button}
-                              variant="link"
-                              eventKey="1"
-                            >
-                              <Button className=" btn btn-primary btn-lg fa fa-pencil "></Button>
-                            </Accordion.Toggle>
-
-                            <Accordion.Collapse eventKey="1">
-                              <Form.Group controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>รายละเอียด</Form.Label>
-                                <Form.Control
-                                  as="textarea"
-                                  rows="3"
-                                  id="cn_notes"
-                                />
-                              </Form.Group>
-                            </Accordion.Collapse>
-                          </Accordion> */}
+                    
                         </div>
                       </td>
                     </tr>
@@ -306,6 +291,8 @@ export default class FromTypeItem extends Component {
                 })}
               </tbody>
             </Table>
+           </Card.Body>
+            
             </Card>
 
           </Col>
