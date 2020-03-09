49f4cf214beeb88223b0c8d7cf8f92a8e2b83b44
waen633 <komet633@gmail.com>
2020-03-09 21:24:51 +0700

    เพิ่มเเก้ไขหน้าFromType

--- src/component/FromTypeItem.js
+++ src/component/FromTypeItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { get, post } from "../service/service";
 import swal from "sweetalert2";
-import './bg.css'
+import "./bg.css";
 import {
   Modal,
   Card,
@@ -15,20 +15,24 @@ import {
   Tab,
   Table,
   Accordion,
-  Form
+  Form,
+  OverlayTrigger,
+  Popover,
+  ButtonToolbar
 } from "react-bootstrap";
-import './bg.css'
+import "./bg.css";
 export default class FromTypeItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      get_data: []
+      get_data: [],
+      useState: 0
     };
   }
   componentDidMount() {
-    this.get_type_item()
+    this.get_type_item();
   }
-
+ 
   get_type_item = async () => {
     try {
       await get("typeName/get_typeName").then(result => {
@@ -56,50 +60,49 @@ export default class FromTypeItem extends Component {
 
   delete_type = data => {
     console.log(data);
-    
-  swal
-    .fire({
-      title: "Are you sure?",
-      text: "ต้องการลบ " + data.TN_name+ " หรือไม่?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
-    })
-    .then(result => {
-      
-      if (result.value) {
-        this.delete_typeName(data);
-      }
-    });
-};
 
-delete_typeName = async data => {
-  try {
-    const obj = {
-      TN_id: data.TN_id
-    };
-    console.log("OBJSD",this.state.TN_id);
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: "ต้องการลบ " + data.TN_name + " หรือไม่?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      })
+      .then(result => {
+        if (result.value) {
+          this.delete_typeName(data);
+        }
+      });
+  };
 
-    await post(obj, "typeName/delete_typeName").then(result => {
-      if (result.success) {
-        swal
-          .fire({
-            icon: "success",
-            title: "Your Type has been deleted.",
-            showConfirmButton: false,
-            timer: 1500
-          })
-          .then(() => {
-            window.location.reload();
-          });
-      }
-    });
-  } catch (error) {
-    alert("delete_typeName: " + error);
-  }
-};
+  delete_typeName = async data => {
+    try {
+      const obj = {
+        TN_id: data.TN_id
+      };
+      console.log("OBJSD", this.state.TN_id);
+
+      await post(obj, "typeName/delete_typeName").then(result => {
+        if (result.success) {
+          swal
+            .fire({
+              icon: "success",
+              title: "Your Type has been deleted.",
+              showConfirmButton: false,
+              timer: 1500
+            })
+            .then(() => {
+              window.location.reload();
+            });
+        }
+      });
+    } catch (error) {
+      alert("delete_typeName: " + error);
+    }
+  };
 
   additem = () => {
     swal
@@ -120,7 +123,6 @@ delete_typeName = async data => {
       });
   };
 
-  
   add_type_item = async () => {
     const obj = {
       TN_name: this.state.itemtype
@@ -147,14 +149,40 @@ delete_typeName = async data => {
       alert("add_type_item" + error);
     }
   };
+
+
+
+  update_type = async (data) => {
+
+    const obj = {
+        TN_id: data.TN_id,
+        TN_name:this.state.itemtype,
+       
+
+    };
+    console.log("gg", obj)
+    try {
+        await post(obj, "typeName/update_type").then(result => {
+            if (result.success) {
+                window.location.reload();
+            } else {
+                swal.fire("", result.error_message, "error");
+            }
+        });
+    } catch (error) {
+        alert("update_type" + error);
+
+    }
+}
+
   render() {
-const {get_data}=this.state
+    const { get_data } = this.state;
     return (
-      <Container  fluid={1} className='bg-fix'>
-        <br/>
-        <Row >
-          <Col sm='3'>
-              <Card  >
+      <Container fluid={1} className="bg-fix">
+        <br />
+        <Row>
+          <Col sm="3">
+            <Card>
               <div className="card-header py-3 bg-info">
                 <div className="col">
                   <h6 className="m-0 font-weight-bold text-light">
@@ -182,76 +210,106 @@ const {get_data}=this.state
                 ></Button>
               </Card.Body>
             </Card>
-            
           </Col>
-          <Col className='text-center'>
-          <Card className="bg-info">
-          <h3 className="m-0 font-weight-bold text-dark ">
-                      ตารางแสดงประเภทของอุปกรณ์ทั้งหมด
-                    </h3>
-          </Card>
-          
-            <Table striped bordered hover variant="info">
+          <Col className="text-center">
+            <Card className="bg-info">
+              <h3 className="m-0 font-weight-bold text-dark ">
+                ตารางแสดงประเภทของอุปกรณ์ทั้งหมด
+              </h3>
+            </Card>
 
+            <Table striped bordered hover variant="info">
               <thead>
-           
                 <tr>
                   <th>ลำดับ</th>
                   <th>ประเภท</th>
                   <th>จำนวนผลิตภัณฑ์</th>
                   <th>เครื่องมือ</th>
                 </tr>
-                
-                </thead>
-                <tbody>
+              </thead>
+              <tbody>
                 {get_data.map((element, index) => {
-                        return (
-                          <tr>
-                          <td>{index + 1}</td>
-                          <td>{element.TN_name}</td>
-                          <td>{element.count_id}</td>
-                          <td >
-                            <div className="btn-toolbar ">
-                              <link
-                                href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"
-                                rel="stylesheet"
-                              ></link>
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{element.TN_name}</td>
+                      <td>{element.count_id}</td>
+                      <td>
+                        <div className="btn-toolbar ">
+                          <link
+                            href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"
+                            rel="stylesheet"
+                          ></link>
 
-                              <button
-                                onClick={() => this.delete_type(element)}
-                                className=" btn btn-danger btn-lg	fas fa-trash-alt "
-                                
-                              />
-                              
-                              <Accordion defaultActiveKey="0">
-                            <Accordion.Toggle  as={Button} variant="link" eventKey="1">
+                          <button
+                            onClick={() => this.delete_type(element)}
+                            className=" btn btn-danger btn-lg	fas fa-trash-alt "
+                          />
+                          <ButtonToolbar>
+                            {["top"].map(placement => (
+                              <OverlayTrigger
+                                trigger="click"
+                                key={placement}
+                                placement={placement}
+                                rootClose='false'
+                                overlay={
+                                  <Popover
+                                    id={`popover-positioned-${placement}`}
+                                  >
+                                    <Popover.Title as="h3">
+                                      เเก้ไขชื่่อประเภท
+                                    </Popover.Title>
+                                    <Popover.Content>
+                                      <input type="textare"
+                                        onChange={this.handleChange}
+                                        id="TN_name"
+                                        defaultValue={element.TN_name}
+                                      ></input>
+                                      <Button
+                                        variant="primary "
+                                        className=" fa fa-plus bg-dark"
+                                        id="TN_id"
+                                        onClick={() => this.update_type(element)}
+                                        size="20"
+                                      ></Button>
+                                    </Popover.Content>
+                                  </Popover>
+                                }
+                              >
                                 <Button className=" btn btn-primary btn-lg fa fa-pencil "></Button>
+                              </OverlayTrigger>
+                            ))}
+                          </ButtonToolbar>
+                          {/* <Accordion defaultActiveKey="0">
+                            <Accordion.Toggle
+                              as={Button}
+                              variant="link"
+                              eventKey="1"
+                            >
+                              <Button className=" btn btn-primary btn-lg fa fa-pencil "></Button>
                             </Accordion.Toggle>
 
-                          
                             <Accordion.Collapse eventKey="1">
-                                <Form.Group controlId="exampleForm.ControlTextarea1">
-                                    <Form.Label>รายละเอียด</Form.Label>
-                                    <Form.Control as="textarea" rows="3" id="cn_notes"  />
-                                </Form.Group>
+                              <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>รายละเอียด</Form.Label>
+                                <Form.Control
+                                  as="textarea"
+                                  rows="3"
+                                  id="cn_notes"
+                                />
+                              </Form.Group>
                             </Accordion.Collapse>
-                        </Accordion>
-
-                            </div>
-                          </td>
-                        </tr>
-                      );
-                    })}
-
-                </tbody>
-         
-
+                          </Accordion> */}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
             </Table>
           </Col>
         </Row>
-
       </Container>
     );
   }
-
 }
